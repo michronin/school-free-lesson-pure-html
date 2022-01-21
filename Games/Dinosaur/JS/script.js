25747abef1b8db0da1b31765ee89c441c858a05c
michronin <mihael.ronin@gmail.com>
2022-01-21 12:01:31 +0200

    Finishing Dino Game

--- Games/Dinosaur/JS/script.js
+++ Games/Dinosaur/JS/script.js
@@ -1,10 +1,14 @@
 import {setupGround, updateGround} from "./ground.js";
+import {getDinoRects, setupDino, updateDino, setDinoLose} from "./dino.js";
+import {getCactusRects, setupCactus, updateCactus} from "./cactus.js";
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
 const SPEED_SCALE_INCREASE = 0.00001;
 
 const worldElem = document.querySelector('[data-world]');
+const scoreElem = document.querySelector('[data-score]');
+const startScreenElem = document.querySelector('[data-start-screen]');
 
 setPixelToWorldScale();
 window.addEventListener("resize", setPixelToWorldScale);
@@ -13,6 +17,7 @@ document.addEventListener("keydown", handleStart, {once: true})
 let lastTime;
 let speedScale;
 let score;
+
 function update(time) {
     if (lastTime == null) {
         lastTime = time;
@@ -22,14 +27,34 @@ function update(time) {
     const delta = time - lastTime;
 
     updateGround(delta, speedScale);
+    updateDino(delta, speedScale);
+    updateCactus(delta, speedScale);
     updateSpeedScale(delta);
     updateScore(delta);
+    if (checkLose()) return handleLose();
     lastTime = time;
     window.requestAnimationFrame(update);
 }
 
+function checkLose() {
+    const dinoRect = getDinoRects();
+    return getCactusRects().some(rect => isCollision(rect, dinoRect))
+}
+
+function isCollision(rect1, rect2) {
+    return rect1.left < rect2.right &&
+        rect1.top < rect2.bottom &&
+        rect1.right > rect2.left &&
+        rect1.bottom > rect2.top;
+}
+
 function updateSpeedScale(delta) {
     speedScale += delta * SPEED_SCALE_INCREASE;
+}
+
+function updateScore(delta) {
+    score += delta * 0.01;
+    scoreElem.textContent = Math.floor(score);
 }
 
 function handleStart() {
@@ -37,7 +62,18 @@ function handleStart() {
     speedScale = 1
     score = 0;
     setupGround();
+    setupDino();
+    setupCactus();
+    startScreenElem.classList.add("hide")
     window.requestAnimationFrame(update);
+}
+
+function handleLose() {
+    setDinoLose();
+    setTimeout(() =>{
+        document.addEventListener("keydown", handleStart, {once: true})
+        startScreenElem.classList.remove("hide");
+    }, 100)
 }
 
 function setPixelToWorldScale() {
