3f3e49155b86c86f0638b9dc10ab2f38d5700a2a
michronin <mihael.ronin@gmail.com>
2022-01-23 21:02:52 +0200

    Fixing ball bag

--- Games/pingPong/JS/script.js
+++ Games/pingPong/JS/script.js
@@ -4,7 +4,7 @@ const ball = new Ball(document.getElementById("ball"));
 let lastTime
 
 function update(time) {
-    if (lastTime !== null) {
+    if (lastTime != null) {
         const delta = time - lastTime;
         ball.update(delta);
     }
