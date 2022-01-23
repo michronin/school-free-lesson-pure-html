2188588fd897a5c4fb7de175eb754442452c4a7d
michronin <mihael.ronin@gmail.com>
2022-01-23 22:08:12 +0200

    Finishing ball

--- Games/pingPong/JS/ball.js
+++ Games/pingPong/JS/ball.js
@@ -1,4 +1,5 @@
 const INITIAL_VELOCITY = 0.025;
+const VELOCITY_INCREASE = 0.000005;
 
 export default class Ball {
     constructor(ballElem) {
@@ -23,6 +24,10 @@ export default class Ball {
         this.ballElem.style.setProperty("--y", value)
     }
 
+    rect() {
+        return this.ballElem.getBoundingClientRect()
+    }
+
     reset() {
         this.x = 50;
         this.y = 50;
@@ -30,7 +35,7 @@ export default class Ball {
         while (Math.abs(this.direction.x) <= 0.2 ||
         Math.abs(this.direction.x) >= 0.9) {
             const heading = randomNumberBetween(0, 2 * Math.PI);
-            this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
+            this.direction = {x: Math.cos(heading), y: Math.sin(heading)}
         }
         this.velocity = INITIAL_VELOCITY;
     }
@@ -38,6 +43,16 @@ export default class Ball {
     update(delta) {
         this.x += this.direction.x * this.velocity * delta;
         this.y += this.direction.y * this.velocity * delta;
+        this.velocity += VELOCITY_INCREASE * delta;
+        const rect = this.rect();
+
+        if (rect.bottom >= window.innerHeight || rect.top <= 0) {
+            this.direction.y *= -1;
+        }
+
+        // if (rect.right >= window.innerWidth || rect.left <= 0) {
+        //     this.direction.x *= -1;
+        // }
     }
 }
 
